14610cd06894beef7d1c08eef0e54c8bac84db74
LiiioV <60844436+LiiioV@users.noreply.github.com>
2021-04-14 22:43:00 +0300

    Update xo.py

--- xo.py
+++ xo.py
@@ -24,11 +24,14 @@ def get_symbol(i, j):
 def is_col(i):
     return a[i][0] == a[i][1] == a[i][2] != -1
 
+
 def is_row(i):
     return a[0][i] == a[1][i] == a[2][i] != -1
 
+
 def is_diag_0():
     return a[0][0] == a[1][1] == a[2][2] != -1
+
 
 def is_diag_1():
     return a[2][0] == a[1][1] == a[0][2] != -1
@@ -40,18 +43,19 @@ def get_line():
             return i + 1
         if is_row(i):
             return i + 4
-    
+
     if is_diag_0():
         return 7
     if is_diag_0():
         return 8
+
 
 def color_col(i):
     global img
     for x in range(H):
         img[(2 * i + 1) * H // 7][(W - H) // 2 + x] = [0, 0, 0, 1]
         img[(2 * i + 1) * H // 7 + 1][(W - H) // 2 + x] = [0, 0, 0, 1]
-        img[(2 * i + 1) * H // 7 - 1][(W - H) // 2 + x] = [0, 0, 0, 1] 
+        img[(2 * i + 1) * H // 7 - 1][(W - H) // 2 + x] = [0, 0, 0, 1]
 
 
 def color_row(i):
@@ -61,6 +65,7 @@ def color_row(i):
         img[x][(W - H) // 2 + (2 * i + 1) * H // 7 + 1] = [0, 0, 0, 1]
         img[x][(W - H) // 2 + (2 * i + 1) * H // 7 - 1] = [0, 0, 0, 1]
 
+
 def color_diag_0():
     global img
     for x in range(H):
@@ -68,6 +73,7 @@ def color_diag_0():
         if x != 0:
             img[x - 1][(W - H) // 2 + x] = [0, 0, 0, 1]
             img[x][(W - H) // 2 + x - 1] = [0, 0, 0, 1]
+
 
 def color_diag_1():
     global img
@@ -101,13 +107,15 @@ def solve():
         color_diag_0()
     elif ans == 8:
         color_diag_1()
-    
+
     cv2.imshow("result_" + filename, png)
     cv2.waitKey(0)
     cv2.imwrite("result_" + filename, png)
 
+
 def main():
     solve()
+
 
 if __name__ == "__main__":
     main()
